e27f8fbb5fa4c74853084dddb48f3c2cc820cdac
JSoi <jsoiworks@gmail.com>
2022-08-17 04:04:11 +0900

    Programmers 81301

--- boj/math/2839.py
+++ boj/math/2839.py
@@ -3,6 +3,7 @@ sugar = int(input())
 # sugar = 3a + 5b
 answer = -1
 for i in range(sugar // 5, -1, -1):
+    # 5kg 봉지로 나올 수 있는 최대
     leftover = sugar - 5 * i
     if leftover % 3 == 0:
         answer = (leftover // 3) + i
--- boj/math/2869.py
+++ boj/math/2869.py
@@ -5,4 +5,4 @@ a, b, v = list(map(int, sys.stdin.readline().rstrip().split()))
 # a 올라감, b 미끄러짐, 높이 v
 movement = a - b
 height = v - b
-print(math.ceil(height / movement))
+print(math.ceil(height / movement)) # 올림해서 가져와야 됩니다
