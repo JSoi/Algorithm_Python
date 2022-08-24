a7c457525d5b83d152902c942a4de375d084d3bc
JSoi <jsoiworks@gmail.com>
2022-08-24 15:30:56 +0900

    Programmers 118666 Solved

--- programmers/118666.py
+++ programmers/118666.py
@@ -2,22 +2,22 @@ import collections
 
 
 def solution(survey, choices):
-    dict = collections.defaultdict(dict)
+    prob = collections.defaultdict(dict)
     chars = ['R', 'T', 'C', 'F', 'J', 'M', 'A', 'N']
     for c in chars:
-        dict[c] = 0
+        prob[c] = 0
     score = [0, -3, -2, -1, 0, 1, 2, 3]  # 스코어 리스트
     for i in range(len(survey)):
         minus = survey[i][0]  # 비동의시의 character
         plus = survey[i][1]  # 동의시의 character
         if score[choices[i]] < 0:
-            dict[minus] += abs(score[choices[i]])
-        else:
-            dict[plus] += abs(score[choices[i]])
-    dict = sorted(dict.items(), key=lambda x: (-x[1], x[0]))
-    answer = dict[0][0] + dict[1][0] + dict[2][0] + dict[3][0]
-    print(answer)
+            prob[minus] += abs(score[choices[i]])
+        elif score[choices[i]] > 0:
+            prob[plus] += abs(score[choices[i]])
+    answer = ''
+    answer += 'T' if prob['T'] > prob['R'] else 'R'
+    answer += 'F' if prob['F'] > prob['C'] else 'C'
+    answer += 'M' if prob['M'] > prob['J'] else 'J'
+    answer += 'N' if prob['N'] > prob['A'] else 'A'
+
     return answer
-
-
-solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5])
