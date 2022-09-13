35866e9719c072f047214a830274e505a40e40e8
JSoi <jsoiworks@gmail.com>
2022-09-13 23:59:14 +0900

    Programmers 12932 Solved

--- programmers/12932_자연수뒤집기.py
+++ programmers/12932_자연수뒤집기.py
@@ -1,7 +1,7 @@
 def solution(n):
     answer = []
     string = str(n)
-    for i in range(len(n),-1):
+    for i in range(len(string) - 1, -1, -1):
         answer.append(int(string[i]))
     print(answer)
     return answer
