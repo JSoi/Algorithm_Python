d3073969889bd35280824b9abfe8494cd533ebda
JSoi <jsoiworks@gmail.com>
2022-09-15 22:09:42 +0900

    Programmers SQL 59047 solved

--- programmers/sql/sqlsheet.sql
+++ programmers/sql/sqlsheet.sql
@@ -36,3 +36,15 @@ SELECT DATETIME AS `시간`
 FROM ANIMAL_INS
 WHERE DATETIME = (SELECT MIN(DATETIME) FROM ANIMAL_INS)
 
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/59047
+이름에 EL 들어가는 동물 찾기
+ */
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE UPPER(NAME) LIKE '%EL%'
+  AND ANIMAL_TYPE = 'Dog'
+ORDER BY NAME
+
+
+
