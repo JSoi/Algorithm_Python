31f265de2b05dc5cf794a3455b9f8bf38897a4e6
JSoi <jsoiworks@gmail.com>
2022-09-16 22:34:33 +0900

    Programmers SQL 59410 solved

--- programmers/sql/sqlsheet.sql
+++ programmers/sql/sqlsheet.sql
@@ -46,5 +46,12 @@ WHERE UPPER(NAME) LIKE '%EL%'
   AND ANIMAL_TYPE = 'Dog'
 ORDER BY NAME
 
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/59410
+NULL 처리하기
+ */
+SELECT ANIMAL_TYPE, IFNULL(NAME, "No name") AS NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
 
 
